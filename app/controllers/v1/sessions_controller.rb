370e0248f2ee130435e65a7cb1ba910cbcc7bb52
Daffa Pradana <daffaarravi@gmail.com>
2025-08-07 20:19:25 +0700

    define auth request method

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -3,12 +3,12 @@ module V1
   class SessionsController < ApplicationController
     def create
       user = User.find_by(name: params.dig(:user, :name))
-      
+
       if user&.authenticate(params.dig(:user, :password))
         token = encode_token({ user_id: user.id })
         render json: { token: token }, status: :ok
       else
-        render json: { error: 'Invalid name or password' }, status: :unauthorized
+        render json: { error: "Invalid name or password" }, status: :unauthorized
       end
     end
 
