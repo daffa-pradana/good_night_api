685106cbb8e376b2a14f9374222fe4ec5a4690a8
Daffa Pradana <daffaarravi@gmail.com>
2025-08-08 16:52:57 +0700

    add more test for sleep trackers

--- spec/requests/v1/sleep_trackers_spec.rb
+++ spec/requests/v1/sleep_trackers_spec.rb
@@ -37,5 +37,16 @@ RSpec.describe "Sleep Trackers", type: :request do
         expect(response).to have_http_status(:ok)
       end
     end
+
+    it "returns correct limited result when paginated params provided" do
+      headers = { params: { page: 1, per_page: 5 }, **as_user(@user_a) }
+      get "/v1/sleep_tracker/followed", **headers
+      expect(response_body.count).to eq(5)
+    end
+
+    it "returns default limit result when paginated params weren't provided" do
+      get "/v1/sleep_tracker/followed", **as_user(@user_a)
+      expect(response_body.count).to eq(20)
+    end
   end
 end
